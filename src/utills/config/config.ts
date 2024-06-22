0ccf1ad105569de9706881efd006fb1935e05f4b
junyoungkim <juny3738@gmail.com>
2024-06-23 02:37:24 +0900

    config edit

--- src/utills/config/config.ts
+++ src/utills/config/config.ts
@@ -2,7 +2,7 @@ import {AppConfigType} from "@/utills/config/config.type";
 import { getEnv } from "./get-value.config";
 import getUserConfig from "./get-user.config";
 
-const GIT_HUB_PERSONAL_ACCESS_TOKEN = getEnv<null | string>('GIT_HUB_PERSONAL_ACCESS_TOKEN',null);
+const GIT_HUB_PERSONAL_ACCESS_TOKEN = getEnv<string>('GIT_HUB_PERSONAL_ACCESS_TOKEN');
 const GIT_HUB_API_VERSION = '2022-11-28';
 const GIT_HUB_API_URL = 'https://api.github.com';
 const SITE_DOMAIN = getUserConfig('domain');
